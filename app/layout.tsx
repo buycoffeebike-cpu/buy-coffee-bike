3419ef9a3c3c6df45cff2beba342ed642a744267
buycoffeebike-cpu <buycoffeebike@vladvik.com>
2026-05-20 19:08:37 -0700

    node -e "const L=require('fs').readFileSync('app/page.jsx','utf8').split(String.fromCharCode(10)); for(let k=314;k<335;k++) console.log((k+1)+': '+L[k]);"

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,7 +42,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}><script dangerouslySetInnerHTML={{ __html: "!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init','1829025697700338');fbq('track','PageView');" }} />{children}</body>
     </html>
   );
 }
